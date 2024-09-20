6a00f6fcdd2fd9db18ae3560d8d5633061e74957
piccasso0407 <happychristmas1995@gmail.com>
2024-09-20 11:32:06 +0900

    Add files via upload

---
+++ 추석버전/pages/2_RAG구축.py
@@ -0,0 +1,320 @@
+import streamlit as st
+import PyPDF2
+from konlpy.tag import Okt
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain_openai import ChatOpenAI
+from langchain.schema import Document
+from string import Template
+import time
+import streamlit as st
+import numpy as np
+from langchain.embeddings import HuggingFaceEmbeddings
+from sklearn.metrics.pairwise import cosine_similarity
+# funcs.py
+from funcs import load_css, load_local_font
+# 페이지 로드
+from info import info_page 
+from form import form_page
+# 외부 CSS 불러오기
+load_css('./style.css')
+# 폰트 설정
+load_local_font('Pretendard', 'C:\Windows\Fonts\Arial.ttf')
+
+st.subheader("1. 파일 읽기")
+st.image("images/주차별메뉴.jpg")
+# 파일 경로
+file_path = './week_menu_by_day.pdf'
+
+# PDF 파일 읽기 함수
+def read_pdf(file_path):
+    pdf_text = []
+    with open(file_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            pdf_text.append(page.extract_text())
+    return pdf_text
+
+# PDF 파일에서 텍스트 불러오기
+pdf_content = read_pdf(file_path)
+len_str = str(len(pdf_content))
+
+st.code(''' 
+            def read_pdf(file_path):
+    pdf_text = []
+    with open(file_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            pdf_text.append(page.extract_text())
+    return pdf_text
+            ''')
+st.text_area("첫 번째 페이지의 내용:", pdf_content[0], height=300)
+st.write(f"전체 페이지 수:{len_str}")
+
+
+# 데이터를 문서 리스트로 변환
+documents = [Document(page_content=text) for text in pdf_content]
+
+
+st.subheader("2. 청크 나누기")
+st.write("청크란?")
+st.write("""rag를 구축하는 데 있어 시스템의 효율성과 성능을 결정하는 중요한 매개 변수 중 하나이다.
+        임베딩에서 알고리즘도 중요하지만 가장 중요한 것중 하나는 어떻게 문서를 파편으로 잘라낼것인가? 이다. 
+        임베딩은 텍스트를 고정된 크기의 벡터로 변경하는 것이기 때문에, 긴 문단을 작은 벡터로 임베딩하게 되면 
+        디테일한 의미를 잃어버릴 수 있고, 반대로 작은 문장으로 임베딩을 하면,
+        검색시 문장에 대한 정확도는 올라가겠지만, 문장이 짧아서 문장 자체가 가지고 있는 정보가 부족하게 된다.
+
+        -출처: https://bcho.tistory.com/1404 [조대협의 블로그:티스토리]""")
+st.markdown("""
+    [LLM chunk size를 결정하는 방법](https://velog.io/@js03210/LLM-chunk-size%EB%A5%BC-%EA%B2%B0%EC%A0%95%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95)
+    """)
+
+st.code('''
+        # 텍스트 청크 분리 함수
+def get_text_chunks(documents, chunk_size, chunk_overlap):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=len
+    )
+    chunks = text_splitter.split_documents(documents)
+    return chunks''')
+
+# 청크 사이즈 및 오버랩 설정
+chunk_size = st.slider("청크 사이즈를 선택하세요:", min_value=100, max_value=5000, value=1000, step=100)
+chunk_overlap = st.slider("청크 오버랩을 선택하세요:", min_value=0, max_value=5000, value=400, step=100)
+
+# 텍스트 청크 분리 함수
+def get_text_chunks(documents, chunk_size, chunk_overlap):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=len
+    )
+    chunks = text_splitter.split_documents(documents)
+    return chunks
+
+if documents:
+    # 슬라이더에서 설정한 chunk_size와 chunk_overlap을 함수에 전달
+    text_chunks = get_text_chunks(documents, chunk_size, chunk_overlap)
+    st.write(f"청크의 개수: {len(text_chunks)}")
+    st.text_area("첫 번째 청크:", text_chunks[0].page_content if text_chunks else "청크가 없습니다.", height=300)
+else:
+    st.write("문서를 읽어오지 못했습니다.")
+
+
+
+st.subheader('3. 벡터스토어')
+st.write("여러가지 벡터스토어")
+st.image("images/벡터스토어.jpg")
+st.write("faiss와 chroma의 차이")
+st.markdown("""
+    [ChromaDB vs FAISS 비교](https://medium.com/@sujathamudadla1213/chromadb-vsfaiss-65cdae3012ab)
+""")
+# 벡터 스토어 생성 함수
+@st.cache_resource
+def get_vectorstore(_text_chunks, selected_model):
+    embeddings = HuggingFaceEmbeddings(
+        model_name=selected_model,
+        model_kwargs={'device': 'cuda'},
+        encode_kwargs={'normalize_embeddings': True}
+    )
+    vectordb = FAISS.from_documents(_text_chunks, embeddings)
+    return vectordb
+st.code('''
+def get_vectorstore(_text_chunks, selected_model):
+    embeddings = HuggingFaceEmbeddings(
+        model_name=selected_model,
+        model_kwargs={'device': 'cuda'},
+        encode_kwargs={'normalize_embeddings': True}
+    )
+    vectordb = FAISS.from_documents(_text_chunks, embeddings)
+    return vectordb
+''')
+st.subheader("4. 임베딩 모델이란?")
+st.image("images/임베딩.jpg")
+st.markdown(
+    """
+    [임베딩이란? (LLM 활용을 위한)](https://www.gnict.org/blog/130/%EA%B8%80/llm%ED%99%9C%EC%9A%A9%EC%9D%84-%EC%9C%84%ED%95%9C-%EC%9E%84%EB%B2%A0%EB%94%A9%EC%9D%B4%EB%9E%80/)
+    """,
+    unsafe_allow_html=True)
+# 사용자가 선택할 임베딩 모델 옵션
+embedding_options = [
+    "jhgan/ko-sroberta-multitask",
+    "sentence-transformers/all-MiniLM-L6-v2",
+    "BAAI/bge-small-en"
+]
+selected_model = st.selectbox("임베딩 모델을 선택하세요:", embedding_options)
+
+
+# 텍스트 청크 생성 및 벡터스토어 생성
+if documents:
+    text_chunks = get_text_chunks(documents, chunk_size, chunk_overlap)
+    vectorstore = get_vectorstore(text_chunks, selected_model)
+    st.write("벡터스토어가 생성되었습니다.")
+
+# 벡터 검색 함수
+def get_word_vector(query, selected_model):
+    embeddings = HuggingFaceEmbeddings(model_name=selected_model)
+    query_embedding = embeddings.embed_query(query)
+    return query_embedding
+
+query = st.text_input("벡터를 확인할 단어를 입력하세요:")
+
+# 단어 벡터 추출 및 표시
+if query:
+    word_vector = get_word_vector(query, selected_model)
+
+    # 벡터의 차원과 앞 5개의 값을 출력
+    vector_dimension = len(word_vector)
+    first_five_values = word_vector[:5]
+    
+    st.write(f"'{query}'의 벡터 차원: {vector_dimension}차원")
+    st.write("벡터의 앞 5개 값:")
+    for i, value in enumerate(first_five_values):
+        st.write(value)
+
+def calculate_cosine_similarity(vec1, vec2):
+    # 코사인 유사도는 벡터 내적을 두 벡터의 노름(크기)으로 나눈 값
+    vec1 = np.array(vec1).reshape(1, -1)
+    vec2 = np.array(vec2).reshape(1, -1)
+    similarity = cosine_similarity(vec1, vec2)[0][0]
+    return similarity
+
+query1 = query
+query2 = st.text_input("두 번째 단어를 입력하세요:")
+
+# 단어 벡터 추출 및 코사인 유사도 계산
+if query1 and query2:
+    word_vector1 = get_word_vector(query1, selected_model)
+    word_vector2 = get_word_vector(query2, selected_model)
+    
+    # 두 벡터 간의 코사인 유사도 계산
+    cosine_sim = calculate_cosine_similarity(word_vector1, word_vector2)
+    
+    # 결과 출력
+    st.write(f"'{query1}'와 '{query2}' 간의 코사인 유사도: {cosine_sim:.4f}")
+
+st.subheader("5. 대화형 체인")
+
+# 대화형 체인 생성 함수 (히스토리 없이)
+def get_conversation_chain(_vectorstore):
+    llm = ChatOpenAI(
+        base_url="http://localhost:1234/v1",
+        api_key="lm-studio",
+        model="teddylee777/EEVE-Korean-Instruct-10.8B-v1.0-gguf",
+        temperature=0.1,
+        streaming=True
+    )
+
+    retriever = _vectorstore.as_retriever(
+        search_type='similarity',
+        search_kwargs={"k": 5},
+        verbose=False
+    )
+
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm, 
+        chain_type="stuff",
+        retriever=retriever, 
+        return_source_documents=True,
+        verbose=False
+    )
+    return conversation_chain
+
+
+
+st.code('''
+def get_conversation_chain(_vectorstore):
+    llm = ChatOpenAI(
+        base_url="http://localhost:1234/v1",
+        api_key="lm-studio",
+        model="teddylee777/EEVE-Korean-Instruct-10.8B-v1.0-gguf",
+        temperature=0.1,
+        streaming=True
+    )
+
+    retriever = _vectorstore.as_retriever(
+        search_type='similarity',
+        search_kwargs={"k": 5},
+        verbose=False
+    )
+
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm, 
+        chain_type="stuff",
+        retriever=retriever, 
+        return_source_documents=True,
+        verbose=False
+    )
+    return conversation_chain
+''')
+# 문서를 청크로 분리
+text_chunks = get_text_chunks(documents, chunk_size, chunk_overlap)
+
+# 벡터 스토어 생성
+vectorstore = get_vectorstore(text_chunks, selected_model)
+
+# 대화형 체인 생성
+conversation_chain = get_conversation_chain(vectorstore)
+
+st.subheader("6. 프롬프트엔지니어링")
+
+st.code('''
+    def get_prompt_template(query):
+    # "고혈압"이 포함된 경우 나트륨 1000 이하 식단 추천
+    if "고혈압" in query:
+        prompt = Template(f"""
+        사용자가 원하는 기간에 맞춰
+        나트륨이 1000mg 이하인 식단을 추천해 주세요.
+        무조건 {pdf_file_path} 또는 {docx_file_path} 자료에서만 찾아줘.
+        """)
+        # 저나트륨 예시 식단
+        breakfast = "오트밀, 무염 견과류"
+        lunch = "현미밥, 저염 닭가슴살"
+        dinner = "채소 샐러드, 그릭 요거트"
+
+    # "식단"이 포함되었지만 "고혈압"은 포함되지 않은 경우 나트륨 제한 없는 식단 추천
+    elif "식단" in query and "고혈압" not in query:
+        prompt = Template(f"""
+        사용자가 원하는 기간에 맞춰
+        나트륨 제한 없이 식단을 추천해 주세요.
+        무조건 {pdf_file_path} 또는 {docx_file_path} 자료에서만 찾아줘.
+        """)
+        # 일반 식단 예시
+        breakfast = "토스트, 계란"
+        lunch = "밥, 돼지고기 불고기"
+        dinner = "파스타, 샐러드"
+
+    # "레시피"가 포함된 경우 레시피 설명
+    elif "레시피" in query:
+        prompt = Template(f"""
+        무조건 {pdf_file_path} 또는 {docx_file_path} 자료에서 레시피를 찾아서 자세히 설명해 줘.
+        """)
+
+    # 그 외의 경우 자유로운 응답 가능
+    else:
+        prompt = Template(f"""
+        {pdf_file_path} 또는 {docx_file_path} 자료뿐만 아니라 추가적인 정보를 포함해서 자유롭게 답변해 줘.
+        """)
+
+    return prompt.substitute(query=query)
+
+    ''')
+
+st.subheader("7. 대화체인 호출")
+
+st.code('''
+chain = conversation_chain
+result = chain({"question": formatted_query, "chat_history": []})
+response = result['answer']
+source_documents = result['source_documents']
+''')
+
+
+
+
---
+++ 추석버전/pages/3_성능평가.py
@@ -0,0 +1,393 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.chat_models import ChatOllama
+from langchain.prompts import ChatPromptTemplate
+from pprint import pprint
+import streamlit as st
+from PyPDF2 import PdfReader
+import docx
+import streamlit as st
+import tiktoken
+from loguru import logger
+import time
+import concurrent.futures
+import pandas as pd
+import json
+import os
+
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader, Docx2txtLoader, UnstructuredPowerPointLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.vectorstores import FAISS
+from langchain.memory import StreamlitChatMessageHistory
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_openai import ChatOpenAI
+
+import tiktoken
+from loguru import logger
+import time
+import concurrent.futures
+import pandas as pd
+import json
+import os
+from transformers import AutoTokenizer
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader, Docx2txtLoader, UnstructuredPowerPointLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.vectorstores import FAISS
+from langchain.memory import StreamlitChatMessageHistory
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_openai import ChatOpenAI
+st.subheader("QnA set")
+qa_data = pd.read_excel('qnaset1.xlsx')
+st.dataframe(qa_data)
+
+import PyPDF2
+import docx  # For reading DOCX files
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain_openai import ChatOpenAI
+from langchain.schema import Document
+from string import Template
+import re
+import os
+import time
+from langchain.prompts import ChatPromptTemplate
+import pandas as pd
+import streamlit as st
+# funcs.py
+from funcs import load_css, load_local_font
+# 페이지 로드
+from info import info_page 
+from form import form_page
+# 외부 CSS 불러오기
+load_css('./style.css')
+# 폰트 설정
+load_local_font('Pretendard', 'C:\Windows\Fonts\Arial.ttf')
+
+st.subheader("내 모델 로드")     
+with st.expander("코드 보기"):
+    st.code('''
+# PDF 파일 읽기 함수
+def read_pdf(file_path):
+    pdf_text = []
+    with open(file_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            pdf_text.append(page.extract_text())
+    return pdf_text
+
+# DOCX 파일 읽기 함수
+def read_docx(file_path):
+    doc = docx.Document(file_path)
+    full_text = []
+    for para in doc.paragraphs:
+        full_text.append(para.text)
+    return '\n'.join(full_text)
+
+# 파일 경로
+pdf_file_path = "./02-우리_몸이_원하는_삼삼한밥상_Ⅸ_본문(화면용_펼침).pdf"
+docx_file_path = "./data/recipes.docx"
+pdf_file_path2 = "./1.pdf"
+
+# PDF 및 DOCX 파일에서 텍스트 불러오기
+pdf_content = read_pdf(pdf_file_path)
+docx_content = read_docx(docx_file_path)
+pdf_content2 = read_pdf(pdf_file_path2)
+
+# 데이터를 딕셔너리 형태로 정리
+data = {
+    "항목": [
+        "이름", "만나이", "성별", "키", "체중", "허리둘레", 
+        "이상지질혈증 여부", "당뇨병 여부", 
+        "음주 여부", "음주 빈도", "1회 주량", 
+        "폭음 횟수", "절주 권고", 
+        "음주 상담 유무",
+        "고강도 운동 여부", "1주일", "1회 운동 시간", 
+        "중강도 운동 여부", "1주일", "1회 운동 시간?", 
+        "걷기, 자전거 운동", "1주일", "총 운동 시간",
+        "음주 점수", "신체활동 점수", "고혈압 확률",
+    ],
+    "값": [
+        "김첨지", 38, "남자", "173.0 cm", "89.0 kg", "88.9 cm",
+        "없음", "없음",
+        "마신다", "월1회정도", "1-2잔",
+        "월 1회 미만", "없음", "없음",
+        "안 한다", "None", "None", 
+        "안 한다", "None", "None",
+        "한다", "5일", "2시간 0분",
+        "9점", "6.39859점", "21.41%",
+    ]
+}
+
+# 데이터프레임 생성
+df = pd.DataFrame(data)
+
+# PDF 및 DOCX 데이터를 문서 리스트로 변환
+documents = []
+
+# pdf_content는 리스트 형태일 가능성이 높으므로 개별 텍스트로 변환
+for text in pdf_content:
+    if isinstance(text, str):
+        documents.append(Document(page_content=text))
+
+# docx_content는 하나의 문자열이므로 바로 추가
+if isinstance(docx_content, str):
+    documents.append(Document(page_content=docx_content))
+
+# pdf_content2도 리스트일 가능성이 있으므로 개별 텍스트로 변환
+for text in pdf_content2:
+    if isinstance(text, str):
+        documents.append(Document(page_content=text))
+
+# 텍스트를 청크로 분리하는 함수
+def get_text_chunks(documents):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size= 100,  # 청크 크기를 더 줄임
+        chunk_overlap=5,
+        length_function=len
+    )
+    chunks = text_splitter.split_documents(documents)
+    return chunks
+
+# 벡터 스토어 생성 함수
+def get_vectorstore(_text_chunks):
+    embeddings = HuggingFaceEmbeddings(
+        model_name="jhgan/ko-sroberta-multitask",
+        model_kwargs={'device': 'cuda'},
+        encode_kwargs={'normalize_embeddings': True}
+    )
+    vectordb = FAISS.from_documents(_text_chunks, embeddings)
+    return vectordb
+
+# 시스템 프롬프트 템플릿 생성
+system_message = """
+data = {
+    "항목": [
+        "이름", "만나이", "성별", "키", "체중", "허리둘레", 
+        "이상지질혈증 여부", "당뇨병 여부", 
+        "술을 마십니까?", "술을 얼마나 자주 마십니까?", "한 번에 술을 얼마나 마십니까?", 
+        "한 번의 술자리에서 7잔 이상을 마시는 횟수", "술을 끊거나 줄이라는 권고를 받은 적이 있습니까?", 
+        "최근 1년 동안 음주 문제로 상담을 받아본 적이 있습니까?",
+        "고강도 운동 여부", "1주일에 며칠 하십니까?", "한 번 할 때 몇 시간 하십니까?", 
+        "중강도 운동 여부", "1주일에 며칠 하십니까?", "한 번 할 때 몇 시간 하십니까?", 
+        "걷기나 자전거를 이용하십니까?", "1주일에 며칠 하십니까?", "대략 몇 시간 움직이십니까?",
+        "음주 점수", "신체활동 점수", "고혈압 확률"
+    ],
+    "값": [
+        "김첨지", 38, "남자", "173.0 cm", "89.0 kg", "88.9 cm",
+        "없음", "없음",
+        "마신다", "월1회정도", "1-2잔",
+        "월 1회 미만", "없음", "없음",
+        "안 한다", "None", "None", 
+        "안 한다", "None", "None",
+        "한다", "5일", "2시간 0분",
+        "9점", "6.39859점", "21.41%",
+    ]
+}
+너는 이 데이터를 읽고 데이터에 알맞은 식단이나 레시피를 소개하주는 영양전문가야.
+"""
+
+# 텍스트 청크 생성
+text_chunks = get_text_chunks(documents)
+
+# 벡터 스토어 생성
+vectorstore = get_vectorstore(text_chunks)
+
+# 대화형 체인 생성 함수
+def get_conversation_chain(_vectorstore):
+    llm = ChatOpenAI(
+        base_url="http://localhost:1234/v1",
+        api_key="lm-studio",
+        model="teddylee777/EEVE-Korean-Instruct-10.8B-v1.0-gguf",
+        temperature=0.3,
+        streaming=True
+    )
+
+    retriever = _vectorstore.as_retriever(
+        search_type='similarity',
+        search_kwargs={"k": 5},
+        verbose=False
+    )
+
+    # 시스템 메시지를 포함하는 프롬프트 템플릿 생성
+    prompt_template = ChatPromptTemplate.from_messages([
+        ("system", system_message),
+        ("user", "{question}")
+    ])
+
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm, 
+        chain_type="stuff",
+        retriever=retriever, 
+        return_source_documents=True,
+        verbose=False
+    )
+    return conversation_chain
+
+# 대화형 체인 생성
+conversation_chain = get_conversation_chain(vectorstore)
+''')
+st.subheader("ragas에 필요한 내 llm 답변 출력")       
+st.code('''
+# 사용자 질의 처리 함수
+def process_user_query(query):
+    formatted_query = get_prompt_template(query)
+    result = conversation_chain({"question": formatted_query, "chat_history": []})
+    response = result['answer']
+    return response
+
+import pandas as pd
+
+# 대화 체인을 통해 질의에 대한 답변과 관련 문서를 한 번에 가져오는 함수
+def process_question(question):
+    # conversation_chain.invoke를 한 번만 호출
+    result = conversation_chain.invoke({
+        "question": question,
+        "chat_history": []
+    })
+    
+    # answer와 contexts를 추출
+    answer = result['answer']  # 오직 'answer'만 추출
+    
+    # 질문이 답변에 포함되어 있으면 제거
+    if question in answer:
+        answer = answer.replace(question, '').strip()  # 질문 제거 및 양쪽 공백 제거
+    
+    contexts = [doc.page_content for doc in result['source_documents']]
+    
+    return answer, contexts
+
+# qa_data의 첫 5개의 질문에 대해서만 'answer'와 'contexts' 열을 채움
+qa_data[['answer', 'contexts']] = qa_data['question'].apply(lambda x: pd.Series(process_question(x)))
+
+# Pandas DataFrame을 Dataset으로 변환
+from datasets import Dataset
+
+dataset = Dataset.from_pandas(qa_data)
+''')
+
+test_middle = pd.read_excel('test_middle.xlsx')
+st.dataframe(test_middle)
+
+st.subheader("ragas")
+st.code('''
+from ragas.metrics import (
+    faithfulness,
+    answer_relevancy,
+    context_recall,
+    context_precision,
+)
+
+from ragas import evaluate
+from ragas.metrics import faithfulness, answer_relevancy, context_recall, context_precision
+from langchain.llms import OpenAI
+from ragas import evaluate
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+# OpenAI 모델 사용
+langchain_llm = OpenAI(model="gpt-3.5-turbo-instruct", openai_api_key="sk-proj-")
+langchain_embeddings = OpenAIEmbeddings(openai_api_key="sk-proj-")
+# 메트릭을 하나씩 실행해보는 코드
+
+result_faithfulness = evaluate(
+    dataset,
+    metrics=[faithfulness],
+    llm=langchain_llm,
+    embeddings=langchain_embeddings,
+    raise_exceptions=False,
+)
+
+result_answer_relevancy = evaluate(
+    dataset,
+    metrics=[answer_relevancy],
+    llm=langchain_llm,
+    embeddings=langchain_embeddings,
+    raise_exceptions=False,
+)
+
+
+result_context_recall = evaluate(
+    dataset,
+    metrics=[context_recall],
+    llm=langchain_llm,
+    embeddings=langchain_embeddings,
+    raise_exceptions=False,
+)
+
+
+result_context_precision = evaluate(
+    dataset,
+    metrics=[context_precision],
+    llm=langchain_llm,
+    embeddings=langchain_embeddings,
+    raise_exceptions=False,
+)
+
+
+# 결과 출력
+print("Faithfulness Result:", result_faithfulness)
+print("Answer Relevancy Result:", result_answer_relevancy)
+print("Context Recall Result:", result_context_recall)
+print("Context Precision Result:", result_context_precision)
+        
+''')
+st.write("참고문헌")
+col1, col2 = st.columns(2)
+
+
+with col1:
+    
+    pdf_url = "https://arxiv.org/pdf/2309.15217"
+
+    # 논문 요약 페이지 URL
+    abs_url = "https://arxiv.org/abs/2309.15217"
+
+    # PDF 파일을 iframe으로 표시
+    st.markdown(f'<iframe src="{pdf_url}" width="100%" height="800px"></iframe>', unsafe_allow_html=True)
+
+    # Arxiv 요약 페이지 링크 추가
+    st.markdown(f'[논문 바로가기]({abs_url})', unsafe_allow_html=True)
+
+with col2:
+    st.image("images/faithfulness.jpg")
+    st.image("images/amnswerrelevancy.jpg")
+    st.image("images/contextrecall.jpg")
+    st.image("images/contextprecision.jpg")
+
+
+
+df = pd.read_csv('test_result.csv')
+
+
+
+
+
+# 데이터프레임을 스트림릿 화면에 출력
+st.subheader("개별점수")
+st.dataframe(df)
+
+st.subheader("종합점수")
+st.image("images/test_final.jpg")
+
+
+
+st.subheader("한계점")
+st.write("1. llm(GPT4)으로 qna 셋을 만들어 그라운드 데이터가 정확하지 않다. ")
+st.write("2. 알 수 없는 이유로 null값이 많이 출력되었다.(토큰 수 제한과 관련있어 보임)")
+st.write("3. 유료모델(GPTAPI) 사용으로 테스트에 한계가 있었다.(테스트 한 번에 5-7달러 가량 소모)")
+
+st.subheader("앞으로의 과제")
+st.write("1. qna 데이터셋을 직접 만들어 오차율을 줄일 것. ")
+st.write("2. null값 줄이기. ")
+st.write("3. ollama모델로 대체할 방법 찾기. ")
