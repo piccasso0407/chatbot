f207e05c448592a950fae02384193db414522449
piccasso0407 <happychristmas1995@gmail.com>
2024-09-20 16:10:02 +0900

    Update 2_RAG구축.py

--- 추석버전/pages/2_RAG구축.py
+++ 추석버전/pages/2_RAG구축.py
@@ -16,7 +16,7 @@ import os
 
 # 페이지 로드
 
-st.markdown("* * *")
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 image_path = os.path.join(current_dir, "images", "주차별메뉴.jpg")
 image_path2 = os.path.join(current_dir, "images", "벡터스토어.jpg")
