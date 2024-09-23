ad9203384c9540cc7e054089eb2356feac1928f5
piccasso0407 <happychristmas1995@gmail.com>
2024-09-23 10:27:31 +0900

    Update 5_고혈압모델.py

--- 추석버전/pages/5_고혈압모델.py
+++ 추석버전/pages/5_고혈압모델.py
@@ -1,2 +1,42 @@
 import streamlit as st
+import PyPDF2
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain_openai import ChatOpenAI
+from langchain.schema import Document
+from string import Template
+import time
+import streamlit as st
+import numpy as np
+from langchain.embeddings import HuggingFaceEmbeddings
+from sklearn.metrics.pairwise import cosine_similarity
+import os
+
+# 페이지 로드
+current_dir = os.path.dirname(os.path.abspath(__file__))
+css_path = os.path.join(current_dir, 'style.css')
+
+# CSS 파일 로드 함수
+def load_css(file_name):
+    if os.path.exists(file_name):
+        with open(file_name, 'r', encoding='utf-8') as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    else:
+        st.error(f"CSS 파일을 찾을 수 없습니다: {file_name}")
+
+# CSS 파일 로드
+load_css(css_path)
+
+# Pretendard 폰트 로드
+st.markdown("""
+    <link href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css" rel="stylesheet">
+    <style>
+        html, body, [class*="css"] {
+            font-family: 'Pretendard', sans-serif;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
 st.write('''고혈압 모델 보러가기''')
