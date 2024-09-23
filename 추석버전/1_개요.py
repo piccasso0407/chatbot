7b6325e79636de9f8fe059dadd8e485af5ae3d61
piccasso0407 <happychristmas1995@gmail.com>
2024-09-23 09:34:40 +0900

    Update 1_개요.py

--- 추석버전/1_개요.py
+++ 추석버전/1_개요.py
@@ -115,9 +115,16 @@ with col2:
         st.markdown("[lmstudio 바로가기](https://lmstudio.ai/)")
 
 st.markdown("* * *")
+image_path4 = os.path.join(current_dir, "images", "model.jpg")
+os.path.exists(image_path4):
+        st.image(image_path4, caption="여러가지 모델", use_column_width=True)
+
 
 st.write('''저는 속도가 빠르고 편리한 lmstudio를 사용했습니다.''')
 
+원하는 모델을 다운로드 받습니다. 
+저는 teddylee777님이 올려주신 
+teddylee777/EEVE-Korean-Instruct-10.8B-v1.0-gguf 모델을 다운로드 받았습니다. 
 
 # 기술 스택 표시
 st.markdown(
