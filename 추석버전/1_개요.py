b0df2d5731f746c4b7a4d07d515003abaa39cdd3
piccasso0407 <happychristmas1995@gmail.com>
2024-09-23 09:56:10 +0900

    Update 1_개요.py

--- 추석버전/1_개요.py
+++ 추석버전/1_개요.py
@@ -116,16 +116,16 @@ with col2:
 
 st.markdown("* * *")
 
-
-st.write('''저는 속도가 빠르고 편리한 lmstudio를 사용했습니다.''')
-image_path5 = os.path.join(current_dir, "images", "model.jpg")
-if os.path.exists(image_path5):
-    st.image(image_path5, caption="여러가지 모델", use_column_width=True)
-
-
-st.write('''원하는 모델을 다운로드 받습니다. 
-저는 teddylee777님이 올려주신 
-teddylee777/EEVE-Korean-Instruct-10.8B-v1.0-gguf 모델을 다운로드 받았습니다. ''')
+col1, col2 = st.columns(2)
+with col1:
+    st.write('''저는 속도가 빠르고 편리한 lmstudio를 사용했습니다.''')
+    image_path5 = os.path.join(current_dir, "images", "model.jpg")
+    if os.path.exists(image_path5):
+        st.image(image_path5, caption="여러가지 모델", use_column_width=True)
+with col2:
+    st.write('''원하는 모델을 다운로드 받습니다. 
+    저는 teddylee777님이 올려주신 
+    teddylee777/EEVE-Korean-Instruct-10.8B-v1.0-gguf 모델을 다운로드 받았습니다. ''')
 
 # 기술 스택 표시
 st.markdown(
