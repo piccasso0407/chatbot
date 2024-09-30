10061c59605a1dc8d06f90309210d11b67ff8f63
piccasso0407 <happychristmas1995@gmail.com>
2024-09-30 14:57:25 +0900

    Update 6_식단추천.py

--- 추석버전/pages/6_식단추천.py
+++ 추석버전/pages/6_식단추천.py
@@ -1,6 +1,75 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.chat_models import ChatOllama
+from langchain.prompts import ChatPromptTemplate
+from pprint import pprint
+import streamlit as st
+from PyPDF2 import PdfReader
+import docx
+import streamlit as st
+import tiktoken
+from loguru import logger
+import time
+import concurrent.futures
+import pandas as pd
+import json
+import os
 
-# 페이지 로드
-load_css('./style.css')
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader, Docx2txtLoader, UnstructuredPowerPointLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.vectorstores import FAISS
+from langchain.memory import StreamlitChatMessageHistory
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_openai import ChatOpenAI
+
+import tiktoken
+from loguru import logger
+import time
+import concurrent.futures
+import pandas as pd
+import json
+import os
+from transformers import AutoTokenizer
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader, Docx2txtLoader, UnstructuredPowerPointLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.vectorstores import FAISS
+from langchain.memory import StreamlitChatMessageHistory
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_openai import ChatOpenAI
+st.set_page_config(layout="wide")
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+css_path = os.path.join(current_dir, 'style.css')
+
+# CSS 파일 로드 함수
+def load_css(file_name):
+    if os.path.exists(file_name):
+        with open(file_name, 'r', encoding='utf-8') as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    else:
+        st.error(f"CSS 파일을 찾을 수 없습니다: {file_name}")
+
+# CSS 파일 로드
+load_css(css_path)
+
+# Pretendard 폰트 로드
+st.markdown("""
+    <link href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css" rel="stylesheet">
+    <style>
+        html, body, [class*="css"] {
+            font-family: 'Pretendard', sans-serif;
+        }
+    </style>
+    """, unsafe_allow_html=True)
 
 # Streamlit 앱의 제목
 st.title("저염식 식단 챗봇")
