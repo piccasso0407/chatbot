b9aceb09603a1ad59edbe9e3dbd7f2e3e9379670
piccasso0407 <happychristmas1995@gmail.com>
2024-09-30 15:12:47 +0900

    Update 6_식단추천.py

--- 추석버전/pages/6_식단추천.py
+++ 추석버전/pages/6_식단추천.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import Chroma
-from langchain_core.output_parsers import StrOutputParser
+from langchain.output_parsers import StrOutputParser 
 from langchain_core.runnables import RunnablePassthrough
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.chat_models import ChatOllama
